63926b96a5bc8dd60e92afddb373e2f37d91a6aa
PoppyRobot <poppyrobot@sina.com>
2015-09-16 16:32:07 +0800

    android_hardware_ril-cm-10.2

---
+++ android_hardware_ril-cm-10.2/libril/ril_event.cpp
@@ -0,0 +1,423 @@
+/* //device/libs/telephony/ril_event.cpp
+**
+** Copyright 2008, The Android Open Source Project
+**
+** Licensed under the Apache License, Version 2.0 (the "License");
+** you may not use this file except in compliance with the License.
+** You may obtain a copy of the License at
+**
+**     http://www.apache.org/licenses/LICENSE-2.0
+**
+** Unless required by applicable law or agreed to in writing, software
+** distributed under the License is distributed on an "AS IS" BASIS,
+** WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+** See the License for the specific language governing permissions and
+** limitations under the License.
+*/
+
+#define LOG_TAG "RILC"
+
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <utils/Log.h>
+#include <ril_event.h>
+#include <string.h>
+#include <sys/time.h>
+#include <time.h>
+
+#include <pthread.h>
+static pthread_mutex_t listMutex;
+#define MUTEX_ACQUIRE() pthread_mutex_lock(&listMutex)
+#define MUTEX_RELEASE() pthread_mutex_unlock(&listMutex)
+#define MUTEX_INIT() pthread_mutex_init(&listMutex, NULL)
+#define MUTEX_DESTROY() pthread_mutex_destroy(&listMutex)
+
+#ifndef timeradd
+#define timeradd(tvp, uvp, vvp)						\
+	do {								\
+		(vvp)->tv_sec = (tvp)->tv_sec + (uvp)->tv_sec;		\
+		(vvp)->tv_usec = (tvp)->tv_usec + (uvp)->tv_usec;       \
+		if ((vvp)->tv_usec >= 1000000) {			\
+			(vvp)->tv_sec++;				\
+			(vvp)->tv_usec -= 1000000;			\
+		}							\
+	} while (0)
+#endif
+
+#ifndef timercmp
+#define timercmp(a, b, op)               \
+        ((a)->tv_sec == (b)->tv_sec      \
+        ? (a)->tv_usec op (b)->tv_usec   \
+        : (a)->tv_sec op (b)->tv_sec)
+#endif
+
+#ifndef timersub
+#define timersub(a, b, res)                           \
+    do {                                              \
+        (res)->tv_sec = (a)->tv_sec - (b)->tv_sec;    \
+        (res)->tv_usec = (a)->tv_usec - (b)->tv_usec; \
+        if ((res)->tv_usec < 0) {                     \
+            (res)->tv_usec += 1000000;                \
+            (res)->tv_sec -= 1;                       \
+        }                                             \
+    } while(0);
+#endif
+
+static fd_set readFds;
+static int nfds = 0;
+
+static struct ril_event * watch_table[MAX_FD_EVENTS];
+static struct ril_event timer_list;
+static struct ril_event pending_list;
+
+#define DEBUG 0
+
+#if DEBUG
+#define dlog(x...) RLOGD( x )
+static void dump_event(struct ril_event * ev)
+{
+    dlog("~~~~ Event %x ~~~~", (unsigned int)ev);
+    dlog("     next    = %x", (unsigned int)ev->next);
+    dlog("     prev    = %x", (unsigned int)ev->prev);
+    dlog("     fd      = %d", ev->fd);
+    dlog("     pers    = %d", ev->persist);
+    dlog("     timeout = %ds + %dus", (int)ev->timeout.tv_sec, (int)ev->timeout.tv_usec);
+    dlog("     func    = %x", (unsigned int)ev->func);
+    dlog("     param   = %x", (unsigned int)ev->param);
+    dlog("~~~~~~~~~~~~~~~~~~");
+}
+#else
+#define dlog(x...) do {} while(0)
+#define dump_event(x) do {} while(0)
+#endif
+
+static void getNow(struct timeval * tv)
+{
+#ifdef HAVE_POSIX_CLOCKS
+    struct timespec ts;
+    clock_gettime(CLOCK_MONOTONIC, &ts);
+    tv->tv_sec = ts.tv_sec;
+    tv->tv_usec = ts.tv_nsec/1000;
+#else
+    gettimeofday(tv, NULL);
+#endif
+}
+
+//Event的初始化，其实就是对3个主要链表的初始化
+static void init_list(struct ril_event * list)
+{
+    memset(list, 0, sizeof(struct ril_event));
+    list->next = list;
+    list->prev = list;
+    list->fd = -1;
+}
+
+//添加Event到链表
+static void addToList(struct ril_event * ev, struct ril_event * list)
+{
+    ev->next = list;
+    ev->prev = list->prev;
+    ev->prev->next = ev;
+    list->prev = ev;
+    dump_event(ev);
+}
+
+//从链表删除Event
+static void removeFromList(struct ril_event * ev)
+{
+    dlog("~~~~ Removing event ~~~~");
+    dump_event(ev);
+
+    ev->next->prev = ev->prev;
+    ev->prev->next = ev->next;
+    ev->next = NULL;
+    ev->prev = NULL;
+}
+
+//从watch链表中删除某个Event
+static void removeWatch(struct ril_event * ev, int index)
+{
+    watch_table[index] = NULL;
+    ev->index = -1;
+
+    FD_CLR(ev->fd, &readFds);
+
+    if (ev->fd+1 == nfds) {
+        int n = 0;
+
+        for (int i = 0; i < MAX_FD_EVENTS; i++) {
+            struct ril_event * rev = watch_table[i];
+
+            if ((rev != NULL) && (rev->fd > n)) {
+                n = rev->fd;
+            }
+        }
+        nfds = n + 1;
+        dlog("~~~~ nfds = %d ~~~~", nfds);
+    }
+}
+
+//处理超时的Event
+static void processTimeouts()
+{
+    dlog("~~~~ +processTimeouts ~~~~");
+    MUTEX_ACQUIRE();
+    struct timeval now;
+    struct ril_event * tev = timer_list.next;
+    struct ril_event * next;
+
+    getNow(&now);
+    // walk list, see if now >= ev->timeout for any events
+
+    dlog("~~~~ Looking for timers <= %ds + %dus ~~~~", (int)now.tv_sec, (int)now.tv_usec);
+	
+	// 如果timer_list中某个事件已经超时  
+    while ((tev != &timer_list) && (timercmp(&now, &tev->timeout, >))) {
+        // Timer expired
+        dlog("~~~~ firing timer ~~~~");
+        next = tev->next;
+
+		// 从timer_list中删除
+        removeFromList(tev);
+
+		// 添加到pending_list中 
+        addToList(tev, &pending_list);
+        tev = next;
+    }
+    MUTEX_RELEASE();
+    dlog("~~~~ -processTimeouts ~~~~");
+}
+
+static void processReadReadies(fd_set * rfds, int n)
+{
+    dlog("~~~~ +processReadReadies (%d) ~~~~", n);
+    MUTEX_ACQUIRE();
+
+    for (int i = 0; (i < MAX_FD_EVENTS) && (n > 0); i++) {
+        struct ril_event * rev = watch_table[i];
+        if (rev != NULL && FD_ISSET(rev->fd, rfds)) {
+			// 添加到pending_list
+            addToList(rev, &pending_list);
+
+			// 如果persist为false才去删除当前Event 
+            if (rev->persist == false) {
+                removeWatch(rev, i);
+            }
+            n--;
+        }
+    }
+
+    MUTEX_RELEASE();
+    dlog("~~~~ -processReadReadies (%d) ~~~~", n);
+}
+
+static void firePending()
+{
+    dlog("~~~~ +firePending ~~~~");
+    struct ril_event * ev = pending_list.next;
+    while (ev != &pending_list) {
+        struct ril_event * next = ev->next;
+		// 删除当前节点
+        removeFromList(ev);
+
+		// 执行其func并把参数传递进去 
+		// 这里的func就是listenCallback
+		// 对于RILJ中过来的Event，我们注册的回调函数是listenCallback
+        ev->func(ev->fd, 0, ev->param);
+        ev = next;
+    }
+    dlog("~~~~ -firePending ~~~~");
+}
+
+static int calcNextTimeout(struct timeval * tv)
+{
+    struct ril_event * tev = timer_list.next;
+    struct timeval now;
+
+    getNow(&now);
+
+    // Sorted list, so calc based on first node
+    if (tev == &timer_list) {
+        // no pending timers
+        return -1;
+    }
+
+    dlog("~~~~ now = %ds + %dus ~~~~", (int)now.tv_sec, (int)now.tv_usec);
+    dlog("~~~~ next = %ds + %dus ~~~~",
+            (int)tev->timeout.tv_sec, (int)tev->timeout.tv_usec);
+    if (timercmp(&tev->timeout, &now, >)) {
+        timersub(&tev->timeout, &now, tv);
+    } else {
+        // timer already expired.
+        tv->tv_sec = tv->tv_usec = 0;
+    }
+    return 0;
+}
+
+//初始化Event链表
+// Initialize internal data structs
+void ril_event_init()
+{
+    MUTEX_INIT();
+
+    FD_ZERO(&readFds);
+    init_list(&timer_list);
+    init_list(&pending_list);
+    memset(watch_table, 0, sizeof(watch_table));
+}
+
+//初始化一个Event
+// Initialize an event
+void ril_event_set(struct ril_event * ev, int fd, bool persist, ril_event_cb func, void * param)
+{
+    dlog("~~~~ ril_event_set %x ~~~~", (unsigned int)ev);
+    memset(ev, 0, sizeof(struct ril_event));
+    ev->fd = fd;
+    ev->index = -1;
+    ev->persist = persist;
+    ev->func = func;
+    ev->param = param;
+    fcntl(fd, F_SETFL, O_NONBLOCK);
+}
+
+//把一个Event添加到watch_table中 
+// Add event to watch list
+void ril_event_add(struct ril_event * ev)
+{
+    dlog("~~~~ +ril_event_add ~~~~");
+    MUTEX_ACQUIRE();
+    for (int i = 0; i < MAX_FD_EVENTS; i++) {
+        if (watch_table[i] == NULL) {
+            watch_table[i] = ev;
+            ev->index = i;
+            dlog("~~~~ added at %d ~~~~", i);
+            dump_event(ev);
+            FD_SET(ev->fd, &readFds);
+            if (ev->fd >= nfds) nfds = ev->fd+1;
+            dlog("~~~~ nfds = %d ~~~~", nfds);
+            break;
+        }
+    }
+    MUTEX_RELEASE();
+    dlog("~~~~ -ril_event_add ~~~~");
+}
+
+//把一个Event添加到timer_list中
+// Add timer event
+void ril_timer_add(struct ril_event * ev, struct timeval * tv)
+{
+    dlog("~~~~ +ril_timer_add ~~~~");
+    MUTEX_ACQUIRE();
+
+    struct ril_event * list;
+    if (tv != NULL) {
+        // add to timer list
+        list = timer_list.next;
+        ev->fd = -1; // make sure fd is invalid
+
+        struct timeval now;
+        getNow(&now);
+        timeradd(&now, tv, &ev->timeout);
+
+        // keep list sorted
+        while (timercmp(&list->timeout, &ev->timeout, < )
+                && (list != &timer_list)) {
+            list = list->next;
+        }
+        // list now points to the first event older than ev
+        addToList(ev, list);
+    }
+
+    MUTEX_RELEASE();
+    dlog("~~~~ -ril_timer_add ~~~~");
+}
+
+//把一个Event从watch_table中删除
+// Remove event from watch or timer list
+void ril_event_del(struct ril_event * ev)
+{
+    dlog("~~~~ +ril_event_del ~~~~");
+    MUTEX_ACQUIRE();
+
+    if (ev->index < 0 || ev->index >= MAX_FD_EVENTS) {
+        MUTEX_RELEASE();
+        return;
+    }
+
+    removeWatch(ev, ev->index);
+
+    MUTEX_RELEASE();
+    dlog("~~~~ -ril_event_del ~~~~");
+}
+
+#if DEBUG
+static void printReadies(fd_set * rfds)
+{
+    for (int i = 0; (i < MAX_FD_EVENTS); i++) {
+        struct ril_event * rev = watch_table[i];
+        if (rev != NULL && FD_ISSET(rev->fd, rfds)) {
+          dlog("DON: fd=%d is ready", rev->fd);
+        }
+    }
+}
+#else
+#define printReadies(rfds) do {} while(0)
+#endif
+
+//主循环  
+void ril_event_loop()
+{
+    int n;
+    fd_set rfds;
+    struct timeval tv;
+    struct timeval * ptv;
+
+	// for循环可以清晰的看到，当RILJ发送数据后，在EventLoop中会依次被timer_list、watch_table、pending_list处理
+ 
+	// 死循环检测Event消息
+    for (;;) {
+
+        // make local copy of read fd_set
+        memcpy(&rfds, &readFds, sizeof(fd_set));
+
+		//计算下一次超时时间
+        if (-1 == calcNextTimeout(&tv)) {
+            // no pending timers; block indefinitely
+            dlog("~~~~ no timers; blocking indefinitely ~~~~");
+
+			// NULL说明select是阻塞模式 
+            ptv = NULL;
+        } else {
+            dlog("~~~~ blocking for %ds + %dus ~~~~", (int)tv.tv_sec, (int)tv.tv_usec);
+			// 非空说明在超时时间内是阻塞模式
+            ptv = &tv;
+        }
+        printReadies(&rfds);
+
+		// 用select去扫描readFds中的所有管道集合，检测RILJ是否有新数据出现
+        n = select(nfds, &rfds, NULL, NULL, ptv);
+        printReadies(&rfds);
+        dlog("~~~~ %d events fired ~~~~", n);
+        if (n < 0) {
+            if (errno == EINTR) continue;
+
+            RLOGE("ril_event: select error (%d)", errno);
+            // bail?
+            return;
+        }
+
+		// 检查超时事件，如果超时，将Event放入pending_list中 
+        // Check for timeouts
+        processTimeouts();
+
+		// 检查watch_table，将Event加入pending_list中
+        // Check for read-ready
+        processReadReadies(&rfds, n);
+
+		//执行pending_list中的Event
+        // Fire away
+        firePending();
+    }
+}
---
+++ android_hardware_ril-cm-10.2/rild/rild.c
@@ -0,0 +1,352 @@
+/* //device/system/rild/rild.c
+**
+** Copyright 2006, The Android Open Source Project
+** Copyright (c) 2012-2013, The Linux Foundation. All rights reserved.
+**
+** Not a Contribution
+**
+** Licensed under the Apache License, Version 2.0 (the "License");
+** you may not use this file except in compliance with the License.
+** You may obtain a copy of the License at
+**
+**     http://www.apache.org/licenses/LICENSE-2.0
+**
+** Unless required by applicable law or agreed to in writing, software
+** distributed under the License is distributed on an "AS IS" BASIS,
+** WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+** See the License for the specific language governing permissions and
+** limitations under the License.
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <dlfcn.h>
+#include <string.h>
+#include <stdint.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <errno.h>
+
+#include <telephony/ril.h>
+#define LOG_TAG "RILD"
+#include <utils/Log.h>
+#include <cutils/properties.h>
+#include <cutils/sockets.h>
+#include <sys/capability.h>
+#include <linux/prctl.h>
+
+#include <private/android_filesystem_config.h>
+#include "hardware/qemu_pipe.h"
+
+#define LIB_PATH_PROPERTY   "rild.libpath"
+#define LIB_ARGS_PROPERTY   "rild.libargs"
+#define MAX_LIB_ARGS        16
+
+static void usage(const char *argv0)
+{
+    fprintf(stderr, "Usage: %s -l <ril impl library> [-- <args for impl library>]\n", argv0);
+    exit(-1);
+}
+
+extern void RIL_register (const RIL_RadioFunctions *callbacks);
+
+extern void RIL_onRequestComplete(RIL_Token t, RIL_Errno e,
+                           void *response, size_t responselen);
+
+extern void RIL_onUnsolicitedResponse(int unsolResponse, const void *data,
+                                size_t datalen);
+
+extern void RIL_requestTimedCallback (RIL_TimedCallback callback,
+                               void *param, const struct timeval *relativeTime);
+
+#ifdef QCOM_HARDWARE
+extern void RIL_setRilSocketName(char * s) __attribute__((weak));
+#endif
+
+
+static struct RIL_Env s_rilEnv = {
+    RIL_onRequestComplete,
+    RIL_onUnsolicitedResponse,
+    RIL_requestTimedCallback
+};
+
+extern void RIL_startEventLoop();
+
+static int make_argv(char * args, char ** argv)
+{
+    // Note: reserve argv[0]
+    int count = 1;
+    char * tok;
+    char * s = args;
+
+    while ((tok = strtok(s, " \0"))) {
+        argv[count] = tok;
+        s = NULL;
+        count++;
+    }
+    return count;
+}
+
+/*
+ * switchUser - Switches UID to radio, preserving CAP_NET_ADMIN capabilities.
+ * Our group, cache, was set by init.
+ */
+void switchUser() {
+    prctl(PR_SET_KEEPCAPS, 1, 0, 0, 0);
+    setuid(AID_RADIO);
+
+    struct __user_cap_header_struct header;
+    struct __user_cap_data_struct cap;
+    header.version = _LINUX_CAPABILITY_VERSION;
+    header.pid = 0;
+    cap.effective = cap.permitted = (1 << CAP_NET_ADMIN) | (1 << CAP_NET_RAW);
+    cap.inheritable = 0;
+    capset(&header, &cap);
+}
+
+int main(int argc, char **argv)
+{
+    const char * rilLibPath = NULL;
+    char **rilArgv;
+    static char * s_argv[MAX_LIB_ARGS] = {NULL};
+    void *dlHandle;
+    const RIL_RadioFunctions *(*rilInit)(const struct RIL_Env *, int, char **);
+    const RIL_RadioFunctions *funcs;
+    char libPath[PROPERTY_VALUE_MAX];
+    unsigned char hasLibArgs = 0;
+    int j = 0;
+    int i;
+    static char clientId[3] = {'0'};
+
+    RLOGD("**RIL Daemon Started**");
+    RLOGD("**RILd param count=%d**", argc);
+    memset(s_argv, 0, sizeof(s_argv));
+
+    s_argv[0] = argv[0];
+
+    umask(S_IRGRP | S_IWGRP | S_IXGRP | S_IROTH | S_IWOTH | S_IXOTH);
+    for (i = 1, j = 1; i < argc ;) {
+        if (0 == strcmp(argv[i], "-l") && (argc - i > 1)) {
+            rilLibPath = argv[i + 1];
+            i += 2;
+        } else if (0 == strcmp(argv[i], "-c") && (argc - i > 1)) {
+            strncpy(clientId, argv[i+1], strlen(clientId));
+            i += 2;
+        } else if (0 == strcmp(argv[i], "--")) {
+            i++;
+            hasLibArgs = 1;
+            memcpy(&s_argv[j], &argv[i], argc-i);
+            break;
+        } else {
+            usage(argv[0]);
+        }
+    }
+
+#ifdef QCOM_HARDWARE
+    if (atoi(clientId) >= MAX_RILDS) {
+        RLOGE("Max Number of rild's supported is: %d", MAX_RILDS);
+        exit(0);
+    }
+    RLOGD ("RIL Client Id:=%s", clientId);
+
+    if (strncmp(clientId, "0", MAX_CLIENT_ID_LENGTH)) {
+        if (RIL_setRilSocketName) {
+            RIL_setRilSocketName(clientId);
+        } else {
+            RLOGE("Trying to instantiate multiple rild sockets without a compatible libril!");
+        }
+    }
+#endif
+
+    if (rilLibPath == NULL) {
+        if ( 0 == property_get(LIB_PATH_PROPERTY, libPath, NULL)) {
+            // No lib sepcified on the command line, and nothing set in props.
+            // Assume "no-ril" case.
+            goto done;
+        } else {
+            rilLibPath = libPath;
+        }
+    }
+
+    /* special override when in the emulator */
+#if 1
+    {
+        static char   arg_device[32];
+        int           done = 0;
+
+		//连接库地址：/system/lib/libreference-ril.so  
+#define  REFERENCE_RIL_PATH  "/system/lib/libreference-ril.so"
+
+        /* first, read /proc/cmdline into memory */
+        char          buffer[1024], *p, *q;
+        int           len;
+        int           fd = open("/proc/cmdline",O_RDONLY);
+
+        if (fd < 0) {
+            RLOGD("could not open /proc/cmdline:%s", strerror(errno));
+            goto OpenLib;
+        }
+
+        do {
+            len = read(fd,buffer,sizeof(buffer)); }
+        while (len == -1 && errno == EINTR);
+
+        if (len < 0) {
+            RLOGD("could not read /proc/cmdline:%s", strerror(errno));
+            close(fd);
+            goto OpenLib;
+        }
+        close(fd);
+
+        if (strstr(buffer, "android.qemud=") != NULL)
+        {
+            /* the qemud daemon is launched after rild, so
+            * give it some time to create its GSM socket
+            */
+            int  tries = 5;
+#define  QEMUD_SOCKET_NAME    "qemud"
+
+            while (1) {
+                int  fd;
+
+                sleep(1);
+
+                fd = qemu_pipe_open("qemud:gsm");
+                if (fd < 0) {
+                    fd = socket_local_client(
+                                QEMUD_SOCKET_NAME,
+                                ANDROID_SOCKET_NAMESPACE_RESERVED,
+                                SOCK_STREAM );
+                }
+                if (fd >= 0) {
+                    close(fd);
+                    snprintf( arg_device, sizeof(arg_device), "%s/%s",
+                                ANDROID_SOCKET_DIR, QEMUD_SOCKET_NAME );
+
+                    memset(s_argv, 0, sizeof(s_argv));
+                    s_argv[1] = "-s";
+                    s_argv[2] = arg_device;
+                    done = 1;
+                    break;
+                }
+                RLOGD("could not connect to %s socket: %s",
+                    QEMUD_SOCKET_NAME, strerror(errno));
+                if (--tries == 0)
+                    break;
+            }
+            if (!done) {
+                RLOGE("could not connect to %s socket (giving up): %s",
+                    QEMUD_SOCKET_NAME, strerror(errno));
+                while(1)
+                    sleep(0x00ffffff);
+            }
+        }
+
+        /* otherwise, try to see if we passed a device name from the kernel */
+        if (!done) do {
+#define  KERNEL_OPTION  "android.ril="
+#define  DEV_PREFIX     "/dev/"
+
+            p = strstr( buffer, KERNEL_OPTION );
+            if (p == NULL)
+                break;
+
+            p += sizeof(KERNEL_OPTION)-1;
+            q  = strpbrk( p, " \t\n\r" );
+            if (q != NULL)
+                *q = 0;
+
+            snprintf( arg_device, sizeof(arg_device), DEV_PREFIX "%s", p );
+            arg_device[sizeof(arg_device)-1] = 0;
+            memset(s_argv, 0, sizeof(s_argv));
+            s_argv[1] = "-d";
+            s_argv[2] = arg_device;
+            done = 1;
+
+        } while (0);
+
+        if (done) {
+            argc = 3;
+            i    = 1;
+            hasLibArgs = 1;
+            rilLibPath = REFERENCE_RIL_PATH;
+            RLOGD("overriding with %s %s", s_argv[1], s_argv[2]);
+        }
+    }
+OpenLib:
+#endif
+	//切换UID为AID_RADIO  
+    switchUser();
+
+	// 打开链接库
+    dlHandle = dlopen(rilLibPath, RTLD_NOW);
+
+    if (dlHandle == NULL) {
+        RLOGE("dlopen failed: %s", dlerror());
+        exit(-1);
+    }
+
+	// 开启EventLoop循环
+    RIL_startEventLoop();
+
+	// 从链接库中（也就是reference-ril.c）寻找RIL_Init函数地址  
+    rilInit = (const RIL_RadioFunctions *(*)(const struct RIL_Env *, int, char **))dlsym(dlHandle, "RIL_Init");
+
+    if (rilInit == NULL) {
+        RLOGE("RIL_Init not defined or exported in %s\n", rilLibPath);
+        exit(-1);
+    }
+
+    if (hasLibArgs) {
+        argc = argc-i+1;
+    } else {
+        static char * newArgv[MAX_LIB_ARGS];
+        static char args[PROPERTY_VALUE_MAX];
+        property_get(LIB_ARGS_PROPERTY, args, "");
+        argc = make_argv(args, s_argv);
+    }
+
+    // Make sure there's a reasonable argv[0]
+    s_argv[0] = argv[0];
+
+    if (argc >= MAX_LIB_ARGS - 2) {
+        RLOGE("Max arguments are passed for rild, args count = %d", argc);
+        exit(0);
+    }
+#ifdef QCOM_HARDWARE
+    /* Client-id is a qualcomm thing */
+    s_argv[argc++] = "-c";
+    s_argv[argc++] = clientId;
+#endif
+
+    RLOGD("RIL_Init argc = %d clientId = %s", argc, s_argv[argc-1]);
+
+	// 初始化过程将s_rilEnv全局变量传递给了reference，然后在reference-ril.c内部将这个值传给了s_rilenv
+	// 而s_rilEnv的各个处理函数是在ril.cpp中实现的
+	// 调用reference-ril.c中的RIL_Init函数进行初始化INIT，同时得到reference-ril的回调函数
+    funcs = rilInit(&s_rilEnv, argc, s_argv);
+
+#ifdef QCOM_HARDWARE
+    if (funcs == NULL) {
+        /* Pre-multi-client qualcomm vendor libraries won't support "-c" either, so
+         * try again without it. This should only happen on ancient qcoms, so raise
+         * a big fat warning
+         */
+        argc -= 2;
+        RLOGE("============= Retrying RIL_Init without a client id. This is only required for very old versions,");
+        RLOGE("============= and you're likely to have more radio breakage elsewhere!");
+        funcs = rilInit(&s_rilEnv, argc, s_argv);
+    }
+#endif
+
+	//注册得到的reference的回调函数
+    RIL_register(funcs);
+
+done:
+
+    while(1) {
+        // sleep(UINT32_MAX) seems to return immediately on bionic
+        sleep(0x00ffffff);
+    }
+}
+
